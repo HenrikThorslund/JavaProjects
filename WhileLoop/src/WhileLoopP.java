660f61337021d0970c8fb9b633184035f2d60d66
Henrik Thorslund <henrik.thorslund@hotmail.se>
2019-05-06 10:57:22 +0200

    Countdown

---
+++ WhileLoop/src/WhileLoopP.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class WhileLoopP {
+	public static void main(String[]args){
+		
+		Scanner tal=new Scanner(System.in);
+		
+		int heltal;
+		
+		System.out.print("Ange ett heltal som du vill räkna ner från:");
+		heltal=tal.nextInt();
+		
+		//while-loop
+		while(heltal>=1){
+			System.out.print(heltal+" ");
+		heltal=heltal-1;
+	
+	}
+		System.exit(0);
+	}
+}
+
